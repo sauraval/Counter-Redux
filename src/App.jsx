678738e98567d87700200ca554d3fb5869b79dd6
sauraval <saurav.l.akshan@gmail.com>
2023-12-27 12:15:04 +0530

    Counter Redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <div style={{height:"100vh"}} className='w-100 d-flex justify-content-center align-items-center'>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
